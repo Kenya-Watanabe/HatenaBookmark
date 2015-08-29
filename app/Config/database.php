f46ee655cac50bf3b4a70ae09195eb4e0e3b1a47
k.watanabe <tackop3511@gmail.com>
2015-08-30 00:38:05 +0900

    little but important commit

--- app/Config/database.php
+++ app/Config/database.php
@@ -73,7 +73,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'konant0820',
+		'password' => '',
 		'database' => 'blog',
 		'prefix' => '',
 		'encoding' => 'utf8',
